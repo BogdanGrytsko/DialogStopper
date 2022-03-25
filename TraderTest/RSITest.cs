c5dda53015858f1e1618fdf3a2a477d086d584a9
BogdanGrytsko <bgrytsko@gmail.com>
2022-03-25 14:53:22 +0200

    Test cleanup

--- TraderTest/RSITest.cs
+++ TraderTest/RSITest.cs
@@ -30,28 +30,24 @@ namespace TraderTest
             var sw = Stopwatch.StartNew();
             var t1 = T1();
             var t2 = T2();
-            var i1 = await t1;
-            var i2 = await t2;
+            await t1;
+            await t2;
             var x1 = sw.Elapsed;
 
             sw.Restart();
-            Task t3;
-            Task t4;
-            var i3 = await T1();
-            var i4 = await T2();
+            await T1();
+            await T2();
             var x2 = sw.Elapsed;
         }
 
-        private static async Task<int> T1()
+        private static async Task T1()
         {
             await Task.Delay(TimeSpan.FromSeconds(1));
-            return 1;
         }
         
-        private static async Task<int> T2()
+        private static async Task T2()
         {
             await Task.Delay(TimeSpan.FromSeconds(1));
-            return 2;
         }
     }    
 }
