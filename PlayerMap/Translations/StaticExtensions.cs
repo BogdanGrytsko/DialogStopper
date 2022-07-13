5394722518a6e86420f0ec3a3fe593876595d4b5
BogdanGrytsko <bgrytsko@gmail.com>
2022-07-13 16:56:41 +0300

    Added creation of translations by template

---
+++ PlayerMap/Translations/StaticExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace PlayerMap.Translations
+{
+    public static class StaticExtensions
+    {
+        public static string ToPascalCase(this string str)
+        {
+            var info = CultureInfo.CurrentCulture.TextInfo;
+            return info.ToTitleCase(str).Replace(" ", string.Empty);
+        }
+    }
+}
--- PlayerMap/Translations/TranslationSortTest.cs
+++ PlayerMap/Translations/TranslationSortTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,6 +18,40 @@ namespace PlayerMap.Translations
             var result = lines.Skip(1).OrderBy(x => x.Split(";").First()).ToList();
             result = new List<string> { lines.First() }.Concat(result).ToList();
             File.WriteAllLines(@"Translations\\Translations_Sorted.csv", result, Encoding.UTF8);
+
+            var template = File.ReadAllText(@"Translations\\template.txt");
+            var languageMap = new Dictionary<int, string>();
+            //language is the key
+            var resultMap = new Dictionary<string, List<string>>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var words = line.Split(";");
+                if (i == 0)
+                {
+                    for (int j = 0; j < words.Length; j++)
+                    {
+                        var language = words[j];
+                        languageMap.Add(j, language);
+                        resultMap.Add(language, new List<string>());
+                    }
+                    continue;
+                }
+                
+                var baseWord = words.First().ToPascalCase();
+                for (int j = 1; j < words.Length; j++)
+                {
+                    var word = words[j];
+                    var replaced = template.Replace("{name}", baseWord).Replace("{value}", word);
+                    resultMap[languageMap[j]].Add(replaced);
+                }
+            }
+
+            foreach (var replaced in resultMap)
+            {
+                var concat = string.Join(Environment.NewLine, replaced.Value);
+                File.WriteAllText(@"Translations\\" + replaced.Key + ".txt", concat);
+            }
         }
     }
 }
