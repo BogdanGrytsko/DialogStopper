897f92f7ce29bcc453c7d6d981e01fd669a55a32
BogdanGrytsko <bgrytsko@gmail.com>
2022-11-01 16:27:22 +0200

    Added enum parsing situations for Eric

---
+++ PlayerMap/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlayerMap
+{
+    public static class ExtensionMethods
+    {
+        public static List<TEnum> GetEnumList<TEnum>() where TEnum : Enum 
+            => ((TEnum[])Enum.GetValues(typeof(TEnum))).ToList();
+        
+        public static T GetAttributeOfType<T>(this Enum enumVal) where T:System.Attribute
+        {
+            var type = enumVal.GetType();
+            var memInfo = type.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+    }
+}
