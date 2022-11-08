f9c6afeb2fff09bba9a55828df7564779d3f0f0c
BogdanGrytsko <bgrytsko@gmail.com>
2022-11-08 16:48:04 +0200

    Fixed NeuroticAnxietyMethod

--- PsychologyCourseWork/NeurotiсAnxietyMethod.cs
+++ PsychologyCourseWork/NeurotiсAnxietyMethod.cs
@@ -4,12 +4,20 @@ namespace PsychologyCourseWork
 {
     public class NeuroticAnxietyMethod
     {
-        private static readonly Dictionary<string, int> pointsMap = new()
+        private static readonly Dictionary<string, double> pointsMap = new()
         {
             { "Так", 1 },
             { "Ні", 0 },
-            { "Не знаю", 0 },
-            { "", -1 },
+            { "Не знаю", 0.5 },
+            { "", 0.5 },
+        };
+        
+        private static readonly Dictionary<string, double> negativePointsMap = new()
+        {
+            { "Так", 0 },
+            { "Ні", 1 },
+            { "Не знаю", 0.5 },
+            { "", 0.5 },
         };
         
         public const int StartIndex = SocialPsychologicalRogersDiamondMethod.EndIndex + 1, AnxietyStartIndex = StartIndex + 42, EndIndex = StartIndex + 89;
@@ -26,15 +34,16 @@ namespace PsychologyCourseWork
                 var point = pointsMap[val];
                 Neurotic += point;
             }
+            var anxietyPlusIdx = AnxietyStartIndex + 37;
             for (int i = AnxietyStartIndex; i < EndIndex; i++)
             {
                 var val = values[i];
-                var point = pointsMap[val];
+                var point = i < anxietyPlusIdx ? pointsMap[val] : negativePointsMap[val];
                 Anxiety += point;
             }
         }
         
-        public int Neurotic { get; init; }
-        public int Anxiety { get; init; }
+        public double Neurotic { get; init; }
+        public double Anxiety { get; init; }
     }
 }
