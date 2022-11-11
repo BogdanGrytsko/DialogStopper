02e17b6ada8a68948d5d37c0dd90f1b1559654f0
BogdanGrytsko <bgrytsko@gmail.com>
2022-11-11 13:19:11 +0200

    SuppliersInElasticButNotInEcoDb

---
+++ TraderTest/SuppliersTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace TraderTest
+{
+    public class SuppliersTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var ecoDbPublished = File.ReadAllLines(@"C:\temp\Ecovadis\SuppliersBUG\SuppliersWithPublishedMetrics.csv");
+            var elasticPublished = File.ReadAllLines(@"C:\temp\Ecovadis\SuppliersBUG\elasticsearchexport.csv");
+
+            var ecoDbSuppliers = GetIds(ecoDbPublished);
+            var elasticSuppliers = GetIds(elasticPublished);
+            var result = elasticSuppliers.Except(ecoDbSuppliers).ToList();
+            File.WriteAllLines(@"C:\temp\Ecovadis\SuppliersBUG\SuppliersInElasticButNotInEcoDb.csv", result.Select(x => x.ToString()));
+        }
+
+        private HashSet<int> GetIds(string[] ecoDbPublished)
+        {
+            return ecoDbPublished.Skip(1).Select(int.Parse).ToHashSet();
+        }
+    }
+}
