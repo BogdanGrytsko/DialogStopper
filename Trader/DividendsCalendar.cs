5a92e6c7a3f98031ea04369bdec8283464155e18
BogdanGrytsko <bgrytsko@gmail.com>
2023-12-10 12:36:26 +0100

    few providers, ended up scraping with hands :) easier to excel spreadsheet directly, from nasdaq

--- Trader/DividendsCalendar.cs
+++ Trader/DividendsCalendar.cs
@@ -1,11 +1,8 @@
-﻿using HtmlAgilityPack;
-
-namespace Trader;
+﻿namespace Trader;
 
 public class DividendsCalendar
 {
     private static string TradingViewUrl = "https://www.tradingview.com/chart/MTdLRWLd/?symbol={0}";
-    private static string SeekingAlphaUrl = "https://seekingalpha.com/symbol/{0}/dividends/history";
 
     private readonly List<string> _symbols;
     private readonly HttpClient _httpClient;
@@ -18,9 +15,27 @@ public class DividendsCalendar
 
     public async Task Scrape()
     {
-        var url = string.Format(SeekingAlphaUrl, "KO");
-        var html = await _httpClient.GetStringAsync(url);
-        var doc = new HtmlDocument();
-        doc.LoadHtml(html);
+    }
+
+    public async Task<string> GetDataFromSA(string symbol)
+    {
+        var seekingAlphaUrl = "https://seekingalpha.com/symbol/{0}/dividends/history";
+        var url = string.Format(seekingAlphaUrl, symbol);
+        return await _httpClient.GetStringAsync(url);
+    }
+
+    public async Task<string> GetDataFromNasdaq(string symbol)
+    {
+        //doesn't work
+        var nasdaqUrl = "https://www.nasdaq.com/market-activity/stocks/{0}/dividend-history";;
+        var url = string.Format(nasdaqUrl, symbol);
+        return await _httpClient.GetStringAsync(url);
+    }
+
+    public async Task<string> GetDataFromStreetInsider(string symbol)
+    {
+        var urlTemplate = "https://www.streetinsider.com/dividend_history.php?q={0}";
+        var url = string.Format(urlTemplate, symbol);
+        return await _httpClient.GetStringAsync(url);
     }
 }
