f3be9efaac094813103d88cf2e897b9388f77eaa
BogdanGrytsko <bgrytsko@gmail.com>
2025-01-08 17:02:57 +0100

    Added Open AI api caller, sample prompt

---
+++ HackatonEV2025/OpenAIApiCaller.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+public class OpenAIApiCaller
+{
+    private const string API_KEY = "0498b625c9cf4de2bf60c00fa0ee5fdd"; // Set your key here
+    private const string ENDPOINT = "https://openai-track06-hackathon.openai.azure.com/openai/deployments/gpt-4o-mini-deployment/chat/completions?api-version=2024-02-15-preview";
+
+    public async Task<OpenAIResponse> Execute(string prompt)
+    {
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("api-key", API_KEY);
+        var payload = new
+        {
+            messages = new object[]
+            {
+                  new {
+                      role = "system",
+                      content = new object[] {
+                          new {
+                              type = "text",
+                              text = "You are an AI assistant that helps people find information."
+                          }
+                      }
+                  },
+                  new {
+                      role = "user",
+                      content = new object[] {
+                          new {
+                              type = "text",
+                              text = prompt
+                          }
+                      }
+                  }
+            },
+            temperature = 0.7,
+            top_p = 0.95,
+            max_tokens = 800,
+            stream = false
+        };
+
+        var response = await httpClient.PostAsync(ENDPOINT, new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"));
+
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var responseData = JsonConvert.DeserializeObject<OpenAIResponse>(content);
+            Console.WriteLine(JsonConvert.DeserializeObject<dynamic>(content));
+            return responseData!;
+        }
+        else
+        {
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Error: {response.StatusCode}, {response.ReasonPhrase}, {responseBody}");
+            throw new Exception(responseBody);
+        }
+    }
+}
---
+++ HackatonEV2025/Program.cs
@@ -0,0 +1,4 @@
+﻿var prompt = "what can I get as a present for BDay for 10 y old boy?";
+
+var caller = new OpenAIApiCaller();
+var response = await caller.Execute(prompt);
